257356d66c1de491922ab2ef1b42e518f71b6124
Patrick Gläßer <p.glaesser@hotmail.de>
2020-01-11 21:43:07 +0100

    deactivate frontend

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import compression from 'compression'; // compresses requests
 import path from 'path';
 
 import './config/passport';
-import { portalRouter } from './routers/portalRouter';
+// import { portalRouter } from './routers/portalRouter';
 import { apiRouter } from './routers/apiRouter';
 import { removeWww } from './utils/redirectToUrl';
 
@@ -21,7 +21,7 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-app.use(express.static(path.join(__dirname, 'src', 'public'), { maxAge: 31557600000 }));
+// app.use(express.static(path.join(__dirname, 'src', 'public'), { maxAge: 31557600000 }));
 app.all(/.*/, removeWww);
 
 app.use('/', apiRouter);
